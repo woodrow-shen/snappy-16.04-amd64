324f9632e6536921f050b9ae1fbefccaa6075414
Woodrow Shen <woodrow.shen@canonical.com>
2017-02-21 15:48:48 +0800

    Update gadget snap version

--- run-stable.sh
+++ run-stable.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo /snap/bin/ubuntu-image --channel stable --image-size 4G --extra-snaps bluez --extra-snaps snapweb --extra-snaps modem-manager --extra-snaps network-manager --extra-snaps tpm2  --extra-snaps uefi-fw-tools -o havasu.img pc.model 
+sudo /snap/bin/ubuntu-image --channel stable --image-size 3800M --extra-snaps bluez --extra-snaps snapweb --extra-snaps modem-manager --extra-snaps network-manager --extra-snaps tpm2  --extra-snaps uefi-fw-tools --extra-snaps locationd --extra-snaps wifi-ap --extra-snaps udisks2 --extra-snaps alsa-utils --extra-snaps pc_16.04-0.8_amd64.snap -o havasu.img pc.model
